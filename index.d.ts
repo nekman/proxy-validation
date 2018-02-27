65899ac75bf863281bac12783d31a85ad06f5b4b
Nils Ekman <nils.ekman@consultant.husqvarnagroup.com>
2018-02-27 12:16:27 +0100

    Added typings + repository url

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,46 @@
-namespace Validator {
+declare namespace Validator {
+
+  export default function ProxyValidatorFactory(validationFields: Object<string, Field>): ProxyValidator;
+
+  export class ProxyValidator {
+    /**
+     * Validates a instance properties by test them on the matching `ValidationFields`.
+     *
+     * @param ignoreUndefinedProperties
+     *  if true, `undefined` properties is ignored and will not be validated.
+     * @param allowExtraProperties
+     *  if true, properties that don't exists in the `ValidationFields` is allowed.
+     */ 
+    validate(ignoreUndefinedProperties = false, allowExtraProperties = false): ProxyValidator;
+
+    /**
+     * Wraps instance in a `Proxy` that intercepts all "set" and assigns on
+     * the objects properties.
+     */
+    initializeValidation(): ProxyValidator;
+
+    /**
+     * Create a instance by set properties from a plain object.
+     * @param obj
+     */
+    static from(obj = {}): ProxyValidator;
+  }
+
   export interface Field {
     min?: number;
     max?: number;
     required?: boolean;
+    /**
+     * Validates the field.
+     *
+     * @param value The value that should be validated
+     * @param field The complete field, with validation properties
+     * @param name The property name
+     */
     validate(value: any, field: Field, name: string | PropertyKey): void;
   }
+}
+
+declare module "proxy-validation" {
+  export = Validator;
 }
